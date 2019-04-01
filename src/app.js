00dd09678f9308aca1c1abd9296ecb698be9acce
karol <karol.vogelgezang@gmail.comgit config --global user.email karol.vogelgezang@gmail.com>
2019-04-01 16:37:46 +0200

    lekcje

--- src/app.js
+++ src/app.js
@@ -1,6 +1,36 @@
-// import './utils';
-import { square, add } from './utils';
+// // import './utils';
+// import substract, { square, add } from './utils';
+// // jakbyś chciał improtwać tylko defaulta to możęsz zrobić tak:
+// // import substract from './utils'
+// // Ponwiaż substract jest defaultem moge zrobić też tak:
+// // import anythingIWant from './utils'
+// // Póżniej tylko robisz:
+// // console.log(anythingIWant(2,4));
+// // wiec z default, nazewnistwo nie ejst ważne
+//
+// console.log('app is running');
+// console.log(square(4));
+// console.log(add(2,4));
+// console.log(substract(2,4));
+//
+//
+// // Importing NPM Modules
+// // install -> import -> use
+//
+// // Improtwacnie po stronie serwera (node.js) i użycie - Server-side usage
+// // const validator = require('validator');
+// // validator.isEmail('chuj@op.pl');
+//
+// // Importowanie za pomocą ES6 (by default)
+// // tylko dla swoich modułów (utworzonych przez nas) potrzebujemy w ścieżce './'
+// import validator from 'validator';
+//
+// console.log(validator.isEmail('test'));
+//
+// // Mogą być róznice w improtowaniu modułów, więc warto to zawsze sprawdzać w dokumentacji
 
-console.log('app is running');
-console.log(square(4));
-console.log(add(2,4));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
--- src/utils.js
+++ src/utils.js
@@ -8,9 +8,21 @@ const add = (a,b) => {
     return a+b;
 }
 
+const substract = (a,b) => a-b;
+
 // Mamy dwa rodzaje eksportów
-// default export
+// default export -  możemy eksportować tylko jedną wartość
 
 // named exports
 // To poniżej to nie obiekt! Tylko w P{ dajemy nazwe zmiennej którą chcemy eksportować
-export { square, add };
+export { square, add, substract as default };
+
+// named exports można też tak
+// export const add = (a,b) => {
+//     return a+b;
+// }
+
+// export default mozna też tak:
+// export default substract;
+// lub
+// export default (a,b) => a-b;
